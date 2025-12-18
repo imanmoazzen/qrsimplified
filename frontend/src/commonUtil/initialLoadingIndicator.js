a9f125337615a34f2070766b85ee7c91829fe89c
Iman Moazzen <imanmoaz@gmail.com>
2025-12-18 15:26:06 -0800

    see if I can see the loader

--- frontend/src/commonUtil/initialLoadingIndicator.js
+++ frontend/src/commonUtil/initialLoadingIndicator.js
@@ -1,5 +1,6 @@
 export const removeInitialLoadingIndicator = () => {
   const loader = document.getElementById("initial-loader");
+  console.log(loader);
   if (loader && !loader.classList.contains("hidden-loader")) {
     loader.classList.add("hidden-loader");
   }
